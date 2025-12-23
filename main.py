5ed94df8c36a024e13ad9f8609fc50b669de2db0
chpachka <mimra883@gmail.com>
2025-12-23 21:18:11 +0300

    Use working GitHub Action

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from kivy.app import App
-from kivy.uix.button import Button
-
-class MyApp(App):
-    def build(self):
-        return Button(text='Hello World!')
-
-MyApp().run()
+print("Hello Android!")
