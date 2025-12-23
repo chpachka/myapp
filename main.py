6087f68a25170cdb78a966ec2d56bc2b3767f749
chpachka <mimra883@gmail.com>
2025-12-23 21:08:48 +0300

    Super simple build

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-from kivy.app import App
-from kivy.uix.button import Button
-
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello APK!')
-
-if __name__ == '__main__':
-    TestApp().run()
+print("Hello Android!")
+input("Press Enter...")
