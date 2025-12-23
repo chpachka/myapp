41e6be235fd8427486527acdab4e8638903e2764
chpachka <mimra883@gmail.com>
2025-12-23 20:21:55 +0300

    Initial commit: Kivy app with GitHub Actions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.button import Button
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello from Termux!')
+        return Button(text='Hello from GitHub Actions!')
 
 if __name__ == '__main__':
     TestApp().run()
